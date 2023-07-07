070360151bfce442aa0240ff78500111f7d869c6
Sushant Jambawale <sushantjambwale@gmail.com>
2023-07-07 19:00:48 +0530

    updated gitignore and mongoDB url

--- server/database/db.js
+++ server/database/db.js
@@ -1,10 +1,8 @@
 import mongoose from "mongoose";
 
 const DBConnection = async () => {
-  // const MONGODB_URI=`mongodb://sushant:sushant@ac-ckoc2ud-shard-00-00.ga1wvtm.mongodb.net:27017,ac-ckoc2ud-shard-00-01.ga1wvtm.mongodb.net:27017,ac-ckoc2ud-shard-00-02.ga1wvtm.mongodb.net:27017/?ssl=true&replicaSet=atlas-mhb5w7-shard-0&authSource=admin&retryWrites=true&w=majority`;
-  const MONGODB_URI = `mongodb+srv://sushant:sushant@file-sharing.ga1wvtm.mongodb.net/?retryWrites=true&w=majority`;
   try {
-    await mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
     console.log("Database Connected Succesfully");
   } catch (error) {
     console.error("Error while connecting with the database", error.message);
